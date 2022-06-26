df2904802b58dbf07a3647b183af2bc306727671
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-06-26 18:19:49 -0300

    InitDeployd

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './redux/store';
 import dotenv from "dotenv";
+import axios from 'axios'
 dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
